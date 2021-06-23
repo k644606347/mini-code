d725283b1519f5deddf5557805a62e58976854c0
k644606347 <k644606347@126.com>
2021-06-23 18:16:35 +0800

    add withLog function

--- unicode/toUTF8.ts
+++ unicode/toUTF8.ts
@@ -1,7 +1,8 @@
 import { isNil } from "lodash";
+import { withLog } from "utils/Tools";
 
-function unicodeToUtf8(str: string) {
-    return str.split('').map(n => {
+const unicodeToUtf8 = withLog((str: string) => {
+    const result = str.split('').map(n => {
         const codePoint = n.codePointAt(0);
 
         if (isNil(codePoint)) {
@@ -16,9 +17,10 @@ function unicodeToUtf8(str: string) {
         // TODO: 后续的utf8转换规则
         return utf8Hex
     }).join('')
-}
+    return result
+})
 
-function encodeURIComponentBy(str: string) {
+const encodeURIComponentBy = withLog((str: string) => {
     const otherSymbols = ['?']
     return unicodeToUtf8(str).split('\\u').filter(Boolean).map(n => {
         const codePointNum = Number('0x' + n)
@@ -28,7 +30,7 @@ function encodeURIComponentBy(str: string) {
         let index = 0;
         let item = '';
         let result = '';
-        while(item = n.substr(index, 2)) {
+        while (item = n.substr(index, 2)) {
             index += 2
             if (/^0+$/.test(item)) continue
             result += ('%' + item.toUpperCase())
@@ -36,9 +38,9 @@ function encodeURIComponentBy(str: string) {
 
         return result
     }).join('')
-}
+})
 
-console.log(unicodeToUtf8(' '))
-console.log(unicodeToUtf8('\u003f'))
-console.log(encodeURIComponent('\u003f\u0076'))
-console.log(encodeURIComponentBy('\u003f\u0076'))
+unicodeToUtf8(' 123')
+unicodeToUtf8('\u003f')
+encodeURIComponent('\u003f\u0076')
+encodeURIComponentBy('\u003f\u0076')
